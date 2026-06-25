7c0ca5053f187af74cb7ec6c9133d75a075e4018
João Hypolito <joaohypolito1@gmail.com>
2026-06-25 13:16:53 -0300

    Initial commit: Motor de combate isolado

---
+++ Batalha_Primeira_Era/Core/Heroes/CombatMage.cs
@@ -0,0 +1,25 @@
+﻿using Batalha_Primeira_Era.Items.Inventory;
+using Batalha_Primeira_Era.Items.Weapons;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Batalha_Primeira_Era.Core.Heroes
+{
+    public class CombatMage : Character, IGreat_Sword<Great_Sword>, ISword<Sword>
+    {
+        public Great_Sword equippedGreat_Sword { get; set;}
+        public Sword equippedSword { get; set;}
+        public CombatMage(string name, float life, int insight, float defense, int strength, int dexterity, int knowledge, Inventory item) :
+        base(name, life, insight, defense, strength, dexterity, knowledge, item)
+        {
+            equippedGreat_Sword = null;
+            equippedSword = null;
+        }
+
+        public override void ReceiveDamage(float damage, BodyPart hitPart)
+        {
+            base.ReceiveDamage(damage, hitPart);
+        }
+    }
+}
---
+++ Batalha_Primeira_Era/Core/Heroes/Wizard.cs
@@ -0,0 +1,17 @@
+﻿using Batalha_Primeira_Era.Items.Inventory;
+using Batalha_Primeira_Era.Items.Weapons;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Batalha_Primeira_Era.Core.Heroes
+{
+    public class Wizard : Character
+    {
+        public Wizard(string name, float life, int insight, float defense, int strength, int dexterity, int knowledge, Inventory item) :
+        base(name, life, insight, defense, strength, dexterity, knowledge, item)
+        {
+        }
+
+    }
+}
